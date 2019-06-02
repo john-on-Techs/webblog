e1fcd2267a9301d762b3a2e69be9a6d5e0c38383
john-on-Techs <gavanayeye@gmail.com>
2019-06-03 00:37:42 +0300

    Error correction on published date

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
+from blog.views import PostIndexView, PostDetailView, PostDelete, PostCreate,PostUpdate
 from django.urls import path
-from . import views
-app_name ='blog'
+
+app_name = 'blog'
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('', PostIndexView.as_view(), name='post_list'),
+    path('post/create/', PostCreate.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/update', PostUpdate.as_view(), name='post_update'),
+    path('post/<int:pk>/delete', PostDelete.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.utils import timezone
+from django.views import generic
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -12,3 +15,35 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, id=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+# ----Class Based Views-------
+class PostIndexView(generic.ListView):
+    model = Post
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+
+class PostCreate(LoginRequiredMixin,generic.CreateView):
+    model = Post
+    fields = ['title', 'text']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdate(LoginRequiredMixin,generic.UpdateView):
+    model = Post
+    fields = ['title', 'text']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostDelete(LoginRequiredMixin,generic.DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
