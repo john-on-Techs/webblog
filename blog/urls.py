26b19e67595bb49253b738aff21a656d5ee3749a
john-on-Techs <gavanayeye@gmail.com>
2019-06-02 23:52:11 +0300

    Adding the Comment Section and showing approved comments for each post

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,17 @@
-from blog.views import PostIndexView, PostDetailView, PostDelete, PostCreate, PostUpdate, PostDraftView,post_publish
+from blog.views import PostIndexView, PostDetailView, PostDelete, PostCreate, PostUpdate, PostDraftView, post_publish, \
+    PostComment, comment_approve, comment_remove
 from django.urls import path
 
 app_name = 'blog'
 urlpatterns = [
-
+    path('post/<int:pk>/comment/', PostComment.as_view(), name='post-comment'),
+    path('comment/<int:pk>/approve/', comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', comment_remove, name='comment_remove'),
     path('post/create/', PostCreate.as_view(), name='post-create'),
     path('post/<int:pk>/delete', PostDelete.as_view(), name='post_delete'),
     path('post/<int:pk>/publish', post_publish, name='post_publish'),
     path('post/<int:pk>/update', PostUpdate.as_view(), name='post_update'),
+
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('drafts/', PostDraftView.as_view(), name='post_draft_list'),
     path('', PostIndexView.as_view(), name='post_list'),
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views import generic
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
@@ -17,11 +17,42 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, id=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
 @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('blog:post_detail', pk=pk)
+
+
+def post_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('blog:post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('blog:post_detail', pk=post_pk)
+
+
+class PostComment(generic.CreateView):
+    model = Comment
+    fields = ['author', 'text']
+    success_url = reverse_lazy('blog:post_list')
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        form.instance.post = post
+        return super().form_valid(form)
 
 
 # ----Class Based Views-------
@@ -34,7 +65,7 @@ class PostIndexView(generic.ListView):
         return Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
 
 
-class PostDraftView(LoginRequiredMixin,generic.ListView):
+class PostDraftView(LoginRequiredMixin, generic.ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'blog/post_draft_list.html'
