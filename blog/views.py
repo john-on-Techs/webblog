88c2f807897505d212cfefbdc2c45b8a2f600394
john-on-Techs <gavanayeye@gmail.com>
2019-06-02 13:21:28 +0300

    Error correction on published date

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
     return render(request, 'blog/post_list.html', {'posts':posts})
