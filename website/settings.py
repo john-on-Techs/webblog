ad73b30ea473695be8e677d53c928032f4f456d7
john-on-Techs <gavanayeye@gmail.com>
2019-06-03 12:06:07 +0300

    setting the medial root and url in setting files

--- website/settings.py
+++ website/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
+
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
