931bac461ec0999d71eeef064d0902a65678de8a
john-on-Techs <gavanayeye@gmail.com>
2019-06-03 12:20:42 +0300

    saving setting files

--- website/settings.py
+++ website/settings.py
@@ -134,9 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
