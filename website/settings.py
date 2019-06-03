13ddce1f3b0147f2d5eb60e0c383fedd397d17f8
john-on-Techs <gavanayeye@gmail.com>
2019-06-03 12:19:18 +0300

    saving setting files

--- website/settings.py
+++ website/settings.py
@@ -135,12 +135,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
 
-<<<<<<< Updated upstream
-
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-=======
+
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
->>>>>>> Stashed changes
