e7f239e1a112f8f73f1c53a6aa14a6faeebe1aa9
john-on-Techs <gavanayeye@gmail.com>
2019-06-08 19:19:41 +0300

    All2

--- website/urls.py
+++ website/urls.py
@@ -27,16 +27,16 @@ sitemaps = {
     'posts': PostSitemap
 }
 
-
 urlpatterns = [
 
     path('accounts/login/', login, name='login'),
     path('accounts/logout/', logout, {'next_page': '/'}, name='logout'),
     path('tinymce/', include('tinymce.urls')),
-    path('admin/', admin.site.urls),
+
     path('select2/', include('django_select2.urls')),
     path('admin/filebrowser/', site.urls),
     path('grappelli/', include('grappelli.urls')),
+    path('admin/', admin.site.urls),
     re_path(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('', include('blog.urls')),
 
